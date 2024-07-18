a0cabe2afec51deada2c3ac8f50250b60dbcefec
PERSONAL <oyattunechi@gmail.com>
2024-07-18 09:45:16 -0600

    DR_SO --- APIGen table edit implementation

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ export default function Home() {
   return (
     <main className="flex h-screen items-center justify-between p-2 md:p-24 tracking-wider">
       <UserTable />
-      {/* <UsersTable /> */}
     </main>
   );
 }
--- components/EditDialog.tsx
+++ components/EditDialog.tsx
@@ -20,9 +20,17 @@ import { Label } from "@/components/ui/label";
 import { FilePenLine } from "lucide-react";
 import { useForm, Controller } from "react-hook-form";
 
-export function EditDialog({ onEdit, table, user, row }: any) {
+export function EditDialog({ user, setData }: any) {
+  const { control, handleSubmit } = useForm({
+    defaultValues: user,
+  });
+
   const onSubmit = (data: any) => {
-    onEdit(data);
+    setData((prevData: any) =>
+      prevData.map((item: any) =>
+        item.id === user.id ? { ...item, ...data } : item
+      )
+    );
   };
 
   return (
@@ -32,80 +40,123 @@ export function EditDialog({ onEdit, table, user, row }: any) {
           variant="link"
           size="icon"
           className="w-full px-2 flex justify-between"
-          onClick={() => {}}
         >
           Edit
-          <FilePenLine className=" h-4 w-4" />
+          <FilePenLine className="h-4 w-4" />
         </Button>
       </DialogTrigger>
       <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>Edit user</DialogTitle>
-          <DialogDescription>
-            Make changes to a user here. Click save when you&apos;re done.
-          </DialogDescription>
-        </DialogHeader>
-        {/* <form onSubmit={handleSubmit(onSubmit)}> */}
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Name
-            </Label>
-            <Input id="name" value="Pedro Duarte" className="col-span-3" />
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <DialogHeader>
+            <DialogTitle>Edit user</DialogTitle>
+            <DialogDescription>
+              Make changes to a user here. Click save when you&apos;re done.
+            </DialogDescription>
+          </DialogHeader>
+          <div className="grid gap-4 py-4">
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="name" className="text-right">
+                Name
+              </Label>
+              <Controller
+                name="name"
+                control={control}
+                render={({ field }) => (
+                  <Input {...field} id="name" className="col-span-3" />
+                )}
+              />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="email" className="text-right">
+                Email
+              </Label>
+              <Controller
+                name="email"
+                control={control}
+                render={({ field }) => (
+                  <Input {...field} id="email" className="col-span-3" />
+                )}
+              />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="role" className="text-right">
+                Role
+              </Label>
+              <Controller
+                name="role"
+                control={control}
+                render={({ field }) => (
+                  <Select
+                    {...field}
+                    onValueChange={field.onChange}
+                    value={field.value}
+                  >
+                    <SelectTrigger className="w-[180px]">
+                      <SelectValue placeholder="Role" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="admin">Admin</SelectItem>
+                      <SelectItem value="user">User</SelectItem>
+                      <SelectItem value="manager">Manager</SelectItem>
+                    </SelectContent>
+                  </Select>
+                )}
+              />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="status" className="text-right">
+                Status
+              </Label>
+              <Controller
+                name="status"
+                control={control}
+                render={({ field }) => (
+                  <Select
+                    {...field}
+                    onValueChange={field.onChange}
+                    value={field.value}
+                  >
+                    <SelectTrigger className="w-[180px]">
+                      <SelectValue placeholder="Status" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="active">Active</SelectItem>
+                      <SelectItem value="inactive">Inactive</SelectItem>
+                      <SelectItem value="pending">Pending</SelectItem>
+                    </SelectContent>
+                  </Select>
+                )}
+              />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="signUpDate" className="text-right">
+                Signup date
+              </Label>
+              <Controller
+                name="signUpDate"
+                control={control}
+                render={({ field }) => (
+                  <Input {...field} id="signUpDate" className="col-span-3" />
+                )}
+              />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="lastLogIn" className="text-right">
+                Last Log in
+              </Label>
+              <Controller
+                name="lastLogIn"
+                control={control}
+                render={({ field }) => (
+                  <Input {...field} id="lastLogIn" className="col-span-3" />
+                )}
+              />
+            </div>
           </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="email" className="text-right">
-              Email
-            </Label>
-            <Input id="email" value="@peduarte" className="col-span-3" />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="role" className="text-right">
-              Role
-            </Label>
-            <Select>
-              <SelectTrigger className="w-[180px]">
-                <SelectValue placeholder="Role" />
-              </SelectTrigger>
-              <SelectContent defaultValue="user">
-                <SelectItem value="admin">Admin</SelectItem>
-                <SelectItem value="user">User</SelectItem>
-                <SelectItem value="manager">Manager</SelectItem>
-              </SelectContent>
-            </Select>
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="status" className="text-right">
-              Status
-            </Label>
-            <Select>
-              <SelectTrigger className="w-[180px]">
-                <SelectValue placeholder="Status" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="active">Active</SelectItem>
-                <SelectItem value="inactive">Inactive</SelectItem>
-                <SelectItem value="pending">Pending</SelectItem>
-              </SelectContent>
-            </Select>
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="signUpDate" className="text-right">
-              Signup date
-            </Label>
-            <Input id="signUpDate" value="@peduarte" className="col-span-3" />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="lastLogIn" className="text-right">
-              Last Log in
-            </Label>
-            <Input id="lastLogIn" value="@peduarte" className="col-span-3" />
-          </div>
-        </div>
-        {/* </form> */}
-        <DialogFooter>
-          <Button type="submit">Save changes</Button>
-        </DialogFooter>
+          <DialogFooter>
+            <Button type="submit">Save changes</Button>
+          </DialogFooter>
+        </form>
       </DialogContent>
     </Dialog>
   );
